e215cef0c11250489e931ff227ba9124a617cae3
Edson Moraes <edson-moraes@outlook.com>
2019-07-08 22:08:30 -0300

    Firsts exercises.
    Signed-off-by: Edson Moraes <edson-moraes@outlook.com>

--- src/MatrizAndVetor.java
+++ src/MatrizAndVetor.java
@@ -1,5 +1,16 @@
 public class MatrizAndVetor {
 	public static void main(String[] args) {
-		
+		// binario
+		int a[] = {1,2,0b101011,4};
+		//octal
+		int c[] = {1,2,053,4};
+		// hexa
+		int d[] = {1,2,0x2B,4};
+		int b[][] = {{1,2,4}, {2,2,1}, {0,43,2}};
+
+		System.out.println(a[3] == b[0][2]);
+		System.out.println("Binário: "+ (a[2]==b[2][1]));
+		System.out.println(" Octal: "+ (c[2]==b[2][1]));
+		System.out.println(" Hexa: "+ (d[2]==b[2][1]));
 	}
 }
